754021cd446f77b37c52a2e1af4e500d57cd7037
hidemune.takahashi <hidemune.takahashi@outlook.com>
2017-09-17 13:04:26 +0900

    Initial Commit

---
+++ LightweightReactive/LightweightReactive.h
@@ -0,0 +1,19 @@
+//
+//  LightweightReactive.h
+//  LightweightReactive
+//
+//  Created by hidemune on 9/17/17.
+//  Copyright © 2017 Hidemune Takahashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LightweightReactive.
+FOUNDATION_EXPORT double LightweightReactiveVersionNumber;
+
+//! Project version string for LightweightReactive.
+FOUNDATION_EXPORT const unsigned char LightweightReactiveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LightweightReactive/PublicHeader.h>
+
+
