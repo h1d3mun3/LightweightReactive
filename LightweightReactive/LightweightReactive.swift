1deff601cede9c80b5d3930ca64119303c906aa7
hidemune.takahashi <hidemune.takahashi@outlook.com>
2017-09-17 13:15:22 +0900

    Added LightweightReactive File to project.

---
+++ LightweightReactive/LightweightReactive.swift
@@ -0,0 +1,9 @@
+//
+//  LightweightReactive.swift
+//  LightweightReactive
+//
+//  Created by hidemune on 9/17/17.
+//  Copyright © 2017 Hidemune Takahashi. All rights reserved.
+//
+
+import Foundation
